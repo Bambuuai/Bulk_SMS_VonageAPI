6be0539b10f55b5424c7fa16a7235a572e15bcd2
AI-General <whitesnowman113@gmail.com>
2024-09-21 10:28:51 +0300

    update env

--- frontend/src/configs/axios-config.js
+++ frontend/src/configs/axios-config.js
@@ -2,34 +2,35 @@ import notify from "@/app/notify";
 import { handleApiErr } from "@/utils";
 import defaultAxios from "axios";
 
-let access_token = false
+let access_token = false;
 if (typeof window !== "undefined") {
-    access_token = window?.localStorage.getItem("auth")
+  access_token = window?.localStorage.getItem("auth");
 }
 const axios = defaultAxios.create({
-    // baseURL: "https://fastapi-9ji4.onrender.com",
-    baseURL: "http://localhost:8000",
-})
+  // baseURL: "https://fastapi-9ji4.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_ROUTE,
+});
 
-axios.interceptors.request.use(
-    (config) => {
-        const access_token = typeof window !== "undefined" ? window.localStorage.getItem("auth") : null;
-        console.log(config)
-        if (access_token && (config.url.startsWith("/admin") || config.url.startsWith("/user")) || config.url === "/profile") {
-            config.headers.Authorization = `Bearer ${access_token}`;
-        }
-        return config;
-    }
-);
+axios.interceptors.request.use((config) => {
+  const access_token = typeof window !== "undefined" ? window.localStorage.getItem("auth") : null;
+  console.log(config);
+  if (
+    (access_token && (config.url.startsWith("/admin") || config.url.startsWith("/user"))) ||
+    config.url === "/profile"
+  ) {
+    config.headers.Authorization = `Bearer ${access_token}`;
+  }
+  return config;
+});
 
 axios.interceptors.response.use(
-    response => {
-        return response
-    },
-    (error) => {
-        handleApiErr(error)
-        return Promise.reject(error);
-    }
-)
+  (response) => {
+    return response;
+  },
+  (error) => {
+    handleApiErr(error);
+    return Promise.reject(error);
+  }
+);
 
-export default axios
+export default axios;
